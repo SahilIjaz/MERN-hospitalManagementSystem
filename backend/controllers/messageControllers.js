ddd40706420baf910a4d73e1c085f7b16505576d
Sahil Ijaz <hssahil2913@gmail.com>
2025-03-26 13:16:42 +0500

    controllers amde

--- backend/controllers/messageControllers.js
+++ backend/controllers/messageControllers.js
@@ -1,7 +1,6 @@
 const catchAsync = require("../utils/catchAsync");
 const appError = require("../utils/appError");
 const Message = require("../models/messageModel");
-
 exports.createMessage = catchAsync(async (req, resizeBy, next) => {
   const message = await Message.create(req.body);
   if (!message) {
--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -7,4 +7,12 @@ router.route("/send-message").post(messageControllers.createMessage);
 router.route("/get-one-message/:id").get(messageControllers.getOneMessage);
 
 router.route("/get-all-messages").get(messageControllers.getAllMessages);
+
+router.route("/update-message/:id").patch(messageControllers.updateMessage);
+
+router
+  .route("/delete-one-message/:id")
+  .delete(messageControllers.deleteMessage);
+
+router.route("/delete-all-messages").delete(messageControllers.getAllMessages);
 module.exports = router;
