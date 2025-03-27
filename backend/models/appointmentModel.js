226d656f4a46d78c184c47487d8079ee896a3793
Sahil Ijaz <hssahil2913@gmail.com>
2025-03-27 13:39:03 +0500

    appointment mode created successfully

---
+++ backend/models/appointmentModel.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const appointmentSchema = new mongoose.Schema(
+  {
+    creator: {
+      type: mongoose.Schema.types.ObjectId,
+      ref: "User",
+    },
+    requested: {
+      type: mongoose.Schema.types.ObjectId,
+      ref: "User",
+    },
+    appointmentData: {
+      type: Date,
+    },
+    appointmentPurpose: {
+      type: String,
+    },
+    appointmentTime: {
+      type: Time,
+    },
+    appointmentType: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+appointmentSchema.pre(/^find/, function (next) {
+  this.populate({});
+  next();
+});
+
+const Appointment = new mongoose.model("Appointment", appointmentSchema);
+module.exports = Appointment;
